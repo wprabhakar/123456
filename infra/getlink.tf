ee6fa6ab179d94d2c0584b714b94cf7c507aa789
walter <kwchess@gmail.com>
2022-12-05 21:35:32 +0800

    BLU-022 Clean deployment

--- infra/getlink.tf
+++ infra/getlink.tf
@@ -29,6 +29,12 @@ resource "aws_s3_object" "getlink_folder" {
   key    = "getlink/"
 }
 
+# data "aws_s3_bucket_object" "getlink_sha256" {
+#   bucket = aws_s3_bucket.lambda_bucket.id
+#   key    = "getlink"
+#   source = "bootstrap"
+# }
+
 resource "aws_lambda_function" "getlink_lambda" {
   depends_on = [
     aws_s3_object.getlink_folder
@@ -36,7 +42,7 @@ resource "aws_lambda_function" "getlink_lambda" {
   function_name = "getlink"
   memory_size = 128
 
-  source_code_hash = filebase64sha256("bootstrap")
+#  s3_object_version = data.aws_s3_object.getlink_sha256.version_id
   handler = "bootstrap"
   runtime = "provided.al2"
 
--- infra/newlink.tf
+++ infra/newlink.tf
@@ -29,11 +29,11 @@ resource "aws_s3_object" "newlink_folder" {
   key    = "newlink/"
 }
 
-data "aws_s3_bucket_object" "newlink_sha256" {
-  bucket = aws_s3_bucket.lambda_bucket.id
-  key    = "newlink"
-  source = "bootstrap"
-}
+# data "aws_s3_bucket_object" "newlink_sha256" {
+#   bucket = aws_s3_bucket.lambda_bucket.id
+#   key    = "newlink"
+#   source = "bootstrap"
+# }
 
 resource "aws_lambda_function" "newlink_lambda" {
   depends_on = [
@@ -44,7 +44,7 @@ resource "aws_lambda_function" "newlink_lambda" {
   # source_code_hash = data.archive_file.lambda_newlink_archive.output_base64sha256
   # filename         = data.archive_file.lambda_newlink_archive.output_path
   # source_code_hash = "${data.aws_s3_bucket_object.newlink_sha256.body}"
-  s3_object_version = data.aws_s3_object.newlink_sha256.version_id
+#  s3_object_version = data.aws_s3_object.newlink_sha256.version_id
 
   handler = "bootstrap"
   runtime = "provided.al2"
