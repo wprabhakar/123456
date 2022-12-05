4b278527abe47e6cc270226e05d490a55379c12a
walter <kwchess@gmail.com>
2022-12-05 13:15:22 +0800

    BLU-020 Fix errros in getlink

--- getlink/src/main.rs
+++ getlink/src/main.rs
@@ -1,17 +1,6 @@
-use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
+use lambda_http::{Body, Error, Request, RequestExt, Response};
 use lambda_runtime::{LambdaEvent, Error};
 use serde::{Serialize, Deserialize};
-
-// async fn newlink(event: LambdaEvent<serde_json::Value>) -> Result<Output, Error> {
-//     let ( payload, _context )  = event.into_parts(); 
-//     print!("Payload  {}!", payload);
-//     let body = payload["body"].as_str().unwrap() ;
-//     let input = serde_json::from_str::<ShortURLs>(body).unwrap() ;
-//     let url = format!("url {}", input.url);
-//     let short_url = format!("{}", nanoid!(9, &ALPHA_NUMERIC));
-
-//     })
-// }
 
 async fn getlink(event: LambdaEvent<serde_json::Value>) -> Result<Response<Body>, Error> {
     let ( payload, _context )  = event.into_parts(); 
@@ -25,7 +14,7 @@ async fn getlink(event: LambdaEvent<serde_json::Value>) -> Result<Response<Body>
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let handler = lambda_runtime::service_fn(newlink);
+    let handler = lambda_runtime::service_fn(getlink);
     lambda_runtime::run(handler).await?;
     Ok(())
 }
