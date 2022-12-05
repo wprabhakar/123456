331fc8936cc25ba1161132bcd002c19db5e71fa7
walter <kwchess@gmail.com>
2022-12-05 14:14:55 +0800

    BLU-020 Fix code

--- getlink/src/main.rs
+++ getlink/src/main.rs
@@ -1,16 +1,4 @@
-//use lambda_http::{ Body, Error };
-//use http::{Request, Response, StatusCode};
-//use lambda_runtime::{service_fn,LambdaEvent};
-// use serde::{Serialize, Deserialize};
 use serde_json::{json};
-// use lambda_apigateway_response::{
-//     http::StatusCode,
-//     types::{
-//         Headers,
-//         MultiValueHeaders,
-//     },
-//     Response,
-// };
 use lambda_runtime::{
     Error as LambdaError,
     LambdaEvent,
@@ -26,18 +14,6 @@ async fn getlink(event: LambdaEvent<serde_json::Value>) -> Result<serde_json::Va
         "headers": { "location": "http://www.google.com"},
         "body": "OK",
     }))
-    // let resp = Response {
-    //     status_code: StatusCode::,
-    //     body: json!({
-    //         "message": "Hello world!",
-    //     }),
-    //     headers: Headers::new(),
-    //     multi_value_headers: MultiValueHeaders::new(),
-    //     is_base64_encoded: true,
-    // };
-
-
-    // Ok(resp)
 }
 
 #[tokio::main]
--- newlink/src/main.rs
+++ newlink/src/main.rs
@@ -30,7 +30,7 @@ async fn main() -> Result<(), Error> {
 
 async fn newlink(event: LambdaEvent<serde_json::Value>) -> Result<serde_json::Value, lambda_runtime::Error>{
     let ( payload, _context )  = event.into_parts(); 
-    print!("Payload {}!", payload);
+    print!(" Payload {}!", payload);
     let body = payload["body"].as_str().unwrap() ;
     let input = serde_json::from_str::<ShortURLs>(body).unwrap() ;
     let url = format!("url {}", input.url);
